20b4a1b0b01531f0bdfd914b117d6806120e7562
rishitathakur01 <rishitat353@gmail.com>
2026-05-28 12:11:37 +0530

    Add files via upload

---
+++ forloop.cpp
@@ -0,0 +1,53 @@
+//FOR LOOOPS
+// for loops say write the stamemtent once and enclose it inside me
+
+// 1st intitialize
+// 2nd condition
+// 3rd incremnet
+#include <iostream>
+using namespace std;
+int main(){
+
+//for (int i=1;i>=10;i=i+1){
+/*print the word 10 times  
+    cout<<"rishita"<<endl;*/
+ /*print 1 to 10
+    cout<<i<<endl;*/
+//}
+
+
+//reverse order
+/*for ( int i=10;i>=0;i=i-1){
+    cout<<i<<endl;
+}*/
+
+///////////////////////////////////////////////////////////////////////
+                             //WHILE
+
+// int i=10;
+// while(i>=0){
+//     cout<<i<<endl;
+//     i=i-1;
+   
+// }
+
+
+// int i=1;
+// while(i<=10){
+//     cout<<"rishita "<<i<<endl;
+//     i=i+1;
+// }
+
+
+///////////////////////////////////////////////////////////////////////
+                          //DO WHILE
+// even if the conditon doesnt allow then also it will rpint ofr one time
+
+int i=2;
+do{
+   cout<<"rishita "<<i<<endl;
+    i=i+1;  
+}while(i<=2);
+    return 0;
+
+}
