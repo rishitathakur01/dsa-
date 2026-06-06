ffe837dc7d87a8a82988d0f0e8456e6d5d13a939
rishitathakur01 <rishitat353@gmail.com>
2026-06-06 22:38:28 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,43 @@
+// FUNCTION
+#include <iostream>
+using namespace std;
+// function are set of code that perform a specific task and can be used again and again
+// they are used to modularize code
+// they increase readabilty
+// used to use same code multiple times
+
+// void->THAT DOESNT RETURN ANYTHING
+// RETURN
+// PARAMETRIZED
+// NON-PARAMETERIZED
+
+
+// void print(string name){
+//     cout << "rishita "<< name<<endl;
+// }
+ int main(){
+// string name;
+// cin>>name;
+//     print(name);
+
+//     string name2;
+//     cin>>name2;
+//     print(name2);
+
+
+// int sum(int a,int b){
+//     int c=a+b;
+//     return c;
+// }
+
+
+int num1 ,num2;
+cin>>num1>>num2;
+int num3 = num1+num2;
+cout<<num3;
+
+
+
+    return 0;
+
+}
