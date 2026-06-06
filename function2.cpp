6c6e600f0a36388a497ac4c6ada6bf826db54320
rishitathakur01 <rishitat353@gmail.com>
2026-06-06 22:39:35 +0530

    Add files via upload

---
+++ function2.cpp
@@ -0,0 +1,84 @@
+//literals->jiskivalue chnage ni hotiii
+
+#include <iostream>
+using namespace std;
+
+// int print(){
+//     cout<<"hello";
+//     return 3;
+// }
+
+// int main(){
+//     // int a=10;
+//     // int b=8;
+//     // print();
+//     // print();
+//     // print();
+//     cout<<print()<<endl;
+//      cout<<print()<<endl;
+//       cout<<print()<<endl;
+//     return 0;
+// }
+
+
+//SUM OF TWO NUMBERS
+// int sum(int num1,int num2){
+//     int s= num1+num2;
+//     return s;
+
+
+//}
+// int main(){
+//   cout<<  sum(23,45)<<endl;
+//   cout<<sum(78,67)<<endl;
+//   cout<<print();
+//     return 0;
+
+// }
+
+
+
+//MIN of two numbers
+
+
+// int min(int a,int b){
+//     if(a<b){
+//         return a;
+//     }
+//     else {
+//     return b;
+// }
+
+// }
+
+// int main(){
+//     cout<<min(12,67);
+//  return 0;
+
+// }
+
+//calculate the sum of numbers from 1 to N
+// int sumN(int n){
+//     int sum=0;
+//     for(int i=1;i<=n;i++){
+//     sum+=i;}
+//     return sum;
+// }
+// int main(){
+//     cout<<sumN(7)<<endl;
+//     cout<<sumN(15)<<endl;
+//     return 0;
+// }
+
+
+//CALCULATE N FACTORIAL
+int fact(int n){
+    for(int i=1;i<=n;i--)
+    return n;
+}
+int main(){
+
+cout<<fact(6)<<endl;
+ return 0;
+
+}
