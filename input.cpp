dc79e7dfa27e79d6f2919ba5575e34c858b3e808
rishitathakur01 <rishitat353@gmail.com>
2026-07-12 22:37:06 +0530

    Refactor input.cpp for improved structure and clarity

--- input.cpp
+++ input.cpp
@@ -41,3 +41,32 @@ int main() {
 
     return 0;
 }
+////////////////////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+int main(){
+
+cout<<"hello world"<<endl;
+ int a =23;
+ cout<<a<<endl;
+ // int 23=344 variable cnnot start with number
+ //int a=67; // cannot have same namew
+ char c='a';//can only store char in single comma  also we cannot store two charecters at once in char
+ cout<<c<<endl;
+
+ bool b1=true;
+ cout<<b1<<endl;
+
+ float f=1.23;
+ cout<<f<<endl;
+
+double d=1.23345;
+cout<<d<<endl;
+
+int b=sizeof(d);
+cout<<"the size of this aboove float variable is "<<b<<endl;
+
+ return 0;
+
+}
