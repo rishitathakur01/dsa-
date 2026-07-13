86ab6100606f74135dae69d4177fc94567c4c083
rishitathakur01 <rishitat353@gmail.com>
2026-07-13 22:40:32 +0530

    Add files via upload

---
+++ lecture2.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// cout<<"hello world"<<endl;
+//  int a =23;
+//  cout<<a<<endl;
+//  // int 23=344 variable cnnot start with number
+//  //int a=67; // cannot have same namew
+//  char c='a';//can only store char in single comma  also we cannot store two charecters at once in char
+//  cout<<c<<endl;
+
+//  bool b1=true;
+//  cout<<b1<<endl;
+
+//  float f=1.23;
+//  cout<<f<<endl;
+
+// double d=1.23345;
+// cout<<d<<endl;
+
+// int b=sizeof(d);
+// cout<<"the size of this aboove float variable is "<<b<<endl;
+
+
+//TYPE CASTING
+// int a ='a';
+// cout<< a;
+// //  char c=98;
+// //  cout<< c;
+//  int b=-1;
+//  cout<<b;
+//  unsigned int c=23;
+//  cout<<c;
+//  //can store positive only...huge value comes out
+  
+//  unsigned int d=-23;
+ //cout<<d;
+
+
+ //OPERATORS
+
+//  int A =2/5;
+//  cout<<A;
+
+
+// double a=2/5;
+// cout<<a;
+
+// double b=2.0/5;
+// cout<<b;
+
+// int a=2;
+// int b=3;
+
+// bool c=a==b;
+// cout<<c;
+
+// bool d=a<=b;
+// cout<<d;
+
+// bool e=a!=b;
+// cout<<e;
+
+int b='z';
+cout<<b;
+
+ return 0;
+
+}
