9e7cb316795078ed707a3455a240ffa0622277cb
rishitathakur01 <rishitat353@gmail.com>
2026-07-15 22:03:58 +0530

    Refactor pattern.cpp to include number printing

--- pattern.cpp
+++ pattern.cpp
@@ -96,5 +96,17 @@ while(i<=n){
     cout<<endl;
     i=i+1;
 }
+    int i=1;
+int count=1;
+while(i<=n){
+    int j=1;
+    while(j<=i){
+        cout<<count;
+        count=count+1;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
     return 0;
 }
