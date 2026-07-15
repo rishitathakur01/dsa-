59e545748e638215c130ca8a0d70ab88ff4bc227
rishitathakur01 <rishitat353@gmail.com>
2026-07-15 21:49:02 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -76,5 +76,15 @@ while(i<=n){
     cout<<endl;
     i=i+1;
 }
+    / int i=1;
+// while(i<=n){
+//     int j=1;
+//     while(j<=i){
+//         cout<<"*";
+//         j=j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+// }
     return 0;
 }
