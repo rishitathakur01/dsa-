db5c57c935a10411ae3c94479cacfb32cb631857
rishitathakur01 <rishitat353@gmail.com>
2026-07-16 16:06:54 +0530

    Refactor pattern.cpp to include various patterns

--- pattern.cpp
+++ pattern.cpp
@@ -140,6 +140,20 @@ while(i<=n){
 //     row=row+1;
 // }
 
+    int i =1;
+
+while(i<=n){
+    int j=1;
+    
+    while(j<=i){
+        cout<<i-j+1;
+        
+        j=j+1;
+
+    }
+    cout<<endl;
+    i=i+1;
+}
     
     return 0;
 }
