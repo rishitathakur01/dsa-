4523025b12bea3782e2e9bff6d7509c6f38f609a
rishitathakur01 <rishitat353@gmail.com>
2026-07-17 23:10:18 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -385,6 +385,38 @@ while(i<=n){
     i=i+1;
 }
 
+int i=1;
+while(i<=n){
+    //print kro space
+    int space=n-i;
+    while (space){
+        cout<<" ";
+        space--;
+    }
+    //print 1st triange
+    int j=1;{
+        while(j<=i){
+            cout<<j;
+            j=j+1;
+        }
+
+    }
+    //print second triangle
+    int start=i-1;
+    while(start){
+        cout<<start;
+        start=start-1;
+
+    }
+    cout<<endl;
+    i=i+1;
+
+}
+
+
+
+
+    
     
     return 0;
 }
