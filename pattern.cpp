c6d53c3710daa40888abce5aba7b7b7d7f68d4d8
rishitathakur01 <rishitat353@gmail.com>
2026-07-17 20:40:39 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -318,5 +318,37 @@ while(i<=n){
     cout<<endl;
     i=i+1;
 }
+    // int i=1;
+// while(i<=n){
+//     int space=i-1;
+//     while(space){
+//         cout<<" ";
+//         space=space-1;
+//     }
+//     int j=1;
+//     while(j<=n-i+1){
+//         cout<<i;
+//         j=j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+// }
+
+    
+int i=1;
+while(i<=n){
+    int space=i-1;
+    while(space){
+        cout<<" ";
+        space=space-1;
+    }
+    int j=i;
+    while(j<=n){
+        cout<<j;
+        j=j+1;
+    }
+    cout<<endl;
+    i=i+1;
+}
     return 0;
 }
