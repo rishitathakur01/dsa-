3672cec8395231954e2d0ebe861efb3b8d195914
rishitathakur01 <rishitat353@gmail.com>
2026-07-18 22:13:39 +0530

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+int main(){
+//     int n;
+//     cin>>n;
+//     cout<<"the value of n is "<<n<<endl;
+
+//     int i=1;
+    
+//     while(i<=n){
+//    int count=1;
+//         int j=1;
+//         while(j<=i){
+            
+//             cout<<count;
+//             count=count+1;
+//             j=j+1;
+//         }
+//         cout<<endl;
+//         i=i+1;
+//     }
+
+
+
+// int a,b=1;
+// a=10;
+// if(++a){
+//     cout<<b;
+// }
+// else{
+//     cout<<(++b);
+// }
+
+
+// int a=1;
+// int b=2;
+
+// if(a-- > 0 && ++b>2){
+//     cout<<"stage1=inside if"<<endl;
+//     }
+// else {
+//     cout<<"stage2-inside else";
+// }
+// cout<<a<<"  "<<b<<endl;
+
+// int a=1;
+// int b=2;
+
+// if(a-- > 0 ||++b>2){
+//     cout<<"stage1=inside if"<<endl;
+//     }
+// else {
+//     cout<<"stage2-inside else";
+// }
+// cout<<a<<"  "<<b<<endl;
+
+// int number=3;
+// cout<<(25*(++number));
+
+// int a=1;
+// int b=a++;
+// int c=++a;
+// cout<<b<<endl;
+// cout<<c<<endl;
+// cout<<a;
+    return 0;
+}
+
+
+
+
+
+
+
+
