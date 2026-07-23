70d683488aab5c0a668ea8137768cd3f4dd894ce
rishitathakur01 <rishitat353@gmail.com>
2026-07-23 23:15:00 +0530

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,5 +1,8 @@
 //STRING
 //every charecter is stired at an index 
+// A string is a sequence of characters used to store and manipulate text.
+// In C++, strings are represented using the string data type from the <string> library.
+// Strings support various operations such as concatenation, comparison, accessing characters, and finding length.
 
 #include <iostream>
 using namespace std;
