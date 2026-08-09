d06d487822ff4d681c2125b85b895d24784bfecc
rishitathakur01 <rishitat353@gmail.com>
2026-08-09 23:42:39 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    cout<<"the value of n is "<<n <<endl;
+    for(int i=1;i<=n;i++){
+        cout<<i;
+    }
+    return 0;
+}
