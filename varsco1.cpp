24b7e799021466ad0ac61939c2cb3028ce572b50
rishitathakur01 <rishitat353@gmail.com>
2026-08-12 00:19:34 +0530

    Add files via upload

---
+++ varsco1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a=3;//decalare a variable before using
+    cout<<a<<endl;
+
+    if(true){
+        cout<<a<<endl;
+    }
+    return 0;
+}
