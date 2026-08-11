6dd3249a95eaee3228c2e83399dc957ac9d88ed3
rishitathakur01 <rishitat353@gmail.com>
2026-08-12 00:20:32 +0530

    Add files via upload

---
+++ varsco4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a=3;
+    cout<<a<<endl;
+// int a=2;
+// cout<<a;//redefinition of a not allowed
+
+
+for (int i=0;i<8;i++){//but in this case it is as it is inside loop
+    cout<<"hii"<<endl;
+}
+    return 0;
+}
+//block ke andr define hone valli chheez ki presence uske andr hi hai
+//ek block mein same variable ni bn skte
