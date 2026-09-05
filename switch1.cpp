a32da69b351c78e55ca9e2449634cd20f7142bf6
rishitathakur01 <rishitat353@gmail.com>
2026-09-05 16:37:05 +0530

    Add files via upload

---
+++ switch1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int num=2;
+    cout<<endl;
+    switch(num){
+        case 1:cout<<"first";
+        break;
+        case 2:cout<<"second";
+        break;
+        default:cout<<"hello";
+    }
+
+    return 0;
+}
