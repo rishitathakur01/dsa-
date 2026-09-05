69ee72f43ba2cee5088b4a9b4bf95f38b14647e7
rishitathakur01 <rishitat353@gmail.com>
2026-09-05 16:38:37 +0530

    Add files via upload

---
+++ conswitch.cpp
@@ -0,0 +1,18 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+
+//     int num=1;
+//     cout<<endl;
+//     switch(num){
+//         case 1:cout<<"first";
+//         continue;
+//         case 2:cout<<"second";
+//         break;
+//         default:cout<<"hello";
+//     }
+
+//     return 0;
+// }
+//continue swicth mein nhi hota kyunki woh sirf loops mein hota hai
+//continue next iteration deta hai pr pr swicth thodi loop hai
