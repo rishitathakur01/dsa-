1e15291118019e491c7d9e6d0f55475dbdc0bee1
rishitathakur01 <rishitat353@gmail.com>
2026-09-07 18:11:15 +0530

    Add files via upload

---
+++ prac16.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n=4;
+int i=1;
+while(i<=n){
+    int space=i-1;
+    while(space){
+        cout<<" ";
+        space--;
+    }
+    int j=i;
+    while(j<=n){
+        cout<<j;
+        j++;
+    }
+    cout<<endl;
+    i++; 
+}
+
+ 
+    return 0;
+}
