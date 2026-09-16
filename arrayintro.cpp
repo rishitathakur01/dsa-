5ea6f39919998d3c703e2aaad439d59646edcfb3
rishitathakur01 <rishitat353@gmail.com>
2026-09-16 18:27:28 +0530

    Add files via upload

---
+++ arrayintro.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void printArray(){
+
+}
+int main(){
+
+    int number[15];
+    //accesing an array
+
+    
+    cout<<number[12]<<endl;
+    //random garbage values print hotu hai
+
+
+    cout<<number[89]<<endl;
+    //to access 89th array we have to maek atlest 90 size array
+    
+    //initialization of an array
+    int arr[3]={5,7,11};
+
+    //accesing using index number
+    cout<<"indexxing "<<arr[1]<<endl;
+
+int arr1[3]={1,2};
+cout<<arr1[2]<<endl;
+
+
+    
+    cout<<"tetsing array"<<endl;
+return 0;
+}
