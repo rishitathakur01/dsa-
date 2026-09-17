5c1765e1705928aa05d4ec54f4d88e7b2caf4bbd
rishitathakur01 <rishitat353@gmail.com>
2026-09-17 21:56:27 +0530

    Add files via upload

---
+++ chararr.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+char ch[5]={'a','b','c','d','e'};
+//cout<<ch; cstyle strimh ki form mein store hoga jiske vjh se i will get abcdeC which will tell the compiler to stop
+for(int i=0;i<5;i++){
+    cout<<ch[i];
+}
+double array[4];
+float arrau1[6];
+bool firstbool[3];
+
+    return 0;
+}
